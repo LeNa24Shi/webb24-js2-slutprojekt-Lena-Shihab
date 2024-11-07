9eed030368ea0bc8a2ab8e79d99f8aeb4ffd0a5e
LeNa24Shi <166696967+LeNa24Shi@users.noreply.github.com>
2024-11-07 08:02:28 +0100

    Delete WebbShopSlutProjektJS2 directory

--- WebbShopSlutProjektJS2/frontend/src/components/WelcomePage.jsx
+++
@@ -1,8 +0,0 @@
-export function WelcomePage({setPage}) {
-    return (
-        <div className="header">
-            <h1>Unlock the Magic of Transformation</h1>
-            <button onClick={()=>setPage("products")}>Shop now</button>
-        </div>
-    )
-}
